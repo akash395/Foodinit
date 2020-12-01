d0705cae77cf51c93d2b8ade10ecaa43bf99f608
akash395 <mumtahidakash@gmail.com>
2020-12-01 12:35:56 -0500

    Added Sign Up,Sign In, and About

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { Component } from "react";
 
-export default function SignUp() {
-    return (
-        <main className = "form">
-            <form type = "submit" style = {{fontSize: '50px', textAlign: 'center'}}>
-                <br />
-                <input type = "text" placeholder = "First Name"/>
-                <br />
-                <br />
-                <input type = "text" placeholder = "Last Name" />
-                <br />
-                <br />
-                <input type = "text" placeholder ="Email" />
-                <br />
-                <br />
-                <input type = "password" placeholder = "Password" />
-                <br />
-                <br />
-                <input type = "password" placeholder = "Confirm Password" />
-                <hr />
-                <button>Submit</button>
 
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div className="Container2">
+            <form>
+                <h3 className="H3class">Sign Up</h3>
+
+                <div className="form-group">
+                    <label>First name</label><br></br>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label><br></br>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label><br></br>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label><br></br>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                <br></br><br></br>
+                <button type="submit" className="SignUpBtn">Sign Up</button><br></br><br></br>
+                <p className="forgot-password text-right">
+                Already registered?   <a href="http://localhost:3000/signin"> sign in</a>
+                </p>
             </form>
-        </main>
-    )
+            </div>
+        );
+    }
 }
+
+
+
+// import React from 'react'
+
+// export default function SignUp() {
+//     return (
+//         <div>
+//             <h3>This is a SignUp page</h3>
+//         </div>
+//     )
+// }
