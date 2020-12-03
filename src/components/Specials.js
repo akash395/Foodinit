963a15f1889364f3cdf0ae7e86b61e35068deb1a
hossn75 <hossn75@gmail.com>
2020-12-02 20:09:52 -0500

    Finalized menu

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import FoodProfile from './FoodProfile'
+import Fish_salad from './../images/Fish_salad.jpg'
+import Mixed_scrambled from './../images/Mixed_scrambled.jpg'
+import Lamb_massaman  from './../images/Lamb_massaman.jpg'
 
 function Specials() {
     return (
-        <div>
-            <h3 style = {{marginLeft: '150px'}}>This is for specials menu</h3>
-        </div>
+       <div>
+           <div className="row">
+            <div className=".col-4">
+            <FoodProfile 
+                imgUrl = {Fish_salad} 
+                name = "Fish Salad"
+                price = "$24.75"
+            />
+            </div>
+             <div className=".col-4">
+            <br></br>
+           
+        <FoodProfile 
+                imgUrl = {Mixed_scrambled}
+                name = "Mixed Scrambled"
+                price = "$29.99"
+            />
+            </div>
+
+            <div className=".col-4">
+            <br></br>
+        <FoodProfile 
+                imgUrl = {Lamb_massaman}
+                name = "Lamb Massaman"
+                price = "$29.75"
+            />
+            </div>
+            </div> 
+            </div>
+             
+            
+       
     )
 }
 
