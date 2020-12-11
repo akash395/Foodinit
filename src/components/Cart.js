b063809f899b91ee566073dbd01e5ba240ea1aca
myumbla3 <38135805+myumbla3@users.noreply.github.com>
2020-12-11 15:48:46 -0500

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -31,6 +31,12 @@ let products =[{
 }
 ]
 
+//lines 35-62 is where we were trying to add the items to cart
+//this was connected to the menu.js so the items shown in the menu TOP PICKS could be added
+//if i got those to be added in the cart i could do every section of the menu
+// the console.log was to see if it was working in the page when you inspect but for some reason it stayed saying "([HMR] Waiting for update signal from WDS…)"
+//i could not check if thitems were being added to the localstorage.
+//i was trying to get the items added to the cart and even if you reloaded the page the items would still be there.
 for (let i = 0; i< carts.length; i++){
     carts[i].addEventListener('click',() => {
         cartNumbers(); 
@@ -58,6 +64,8 @@ function cartNumbers(){
     localStorage.setItem('cartNumbers',)*/
 
 }
+
+
 
 export class Cart extends Component {
     constructor (props){
