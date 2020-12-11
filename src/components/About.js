ad480f7cb140e5058fbbbae1398c43bbd3d90404
Anvinhtr <74251694+Anvinhtr@users.noreply.github.com>
2020-12-10 20:55:36 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+
 function About() {
     return (
         <div>
-            <h3>This is About page.</h3>
+            <center>
+            <h1>About Us</h1>
+            <h2>FoodInIt was created to provide a greater quality in food that will satisfy consumers. <br/>
+            Food can be delivered right to your doorstep or you can simply pick it up here. <br/>
+            We strive to provide the utmost quality and hope that everyone is satisfied with our food! <br/>
+            
+            </h2>
+            </center>
+            
         </div>
     )
 }
-
 export default About
